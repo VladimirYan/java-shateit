97e58895617de9ed717f5c5fa6764b084f725909
VladimirYan <167676012+VladimirYan@users.noreply.github.com>
2025-01-08 21:24:47 +0700

    Спринт 15 фикс чекстайл

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
